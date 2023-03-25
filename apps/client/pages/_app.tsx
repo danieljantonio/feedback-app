9f05dad0ce4f44db7949a7c037ee092778a12e4d
Daniel J. Antonio <djedidiaha@gmaio.com>
2023-03-25 14:23:47 +0700

    add react query

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -5,15 +5,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <title>Product - Review</title>
       </Head>
       <main className="app" style={{ width: '100%' }}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </main>
     </>
   );
